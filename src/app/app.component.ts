8952dcecfa12dd11055bbf00a17791f1cfca00c1
MARIA DE LOS ANGELES PINA MIRA <mapm16@clL27-3>
2017-01-27 20:43:30 +0100

    primera subida 27/01/17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SwapiService} from "./core/swapi.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+  people = [];
+
+  constructor (private swapiService:SwapiService) {
+
+  }
+
+  ngOnInit(): void {
+    this.swapiService
+      .getPeopleList()
+      .subscribe((results) => this.people = results);
+  }
 }
